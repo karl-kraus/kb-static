36aa65118386d2d77af678e8d08cb60d60b1a126
csae8092 <p.andorfer@gmail.com>
2026-03-11 13:31:33 +0100

    feat: new chart and refactoring

--- pyscripts/config.py
+++ pyscripts/config.py
@@ -4,3 +4,4 @@ HTML_DIR = "html"
 HTML_DATA_DIR = os.path.join(HTML_DIR, "js", "charts", "data")
 LISTBIBL = os.path.join("data", "indices", "listbibl.xml")
 LISTPERSON = os.path.join("data", "indices", "listperson.xml")
+LISTPLACE = os.path.join("data", "indices", "listplace.xml")
--- pyscripts/make_charts_data.py
+++ pyscripts/make_charts_data.py
@@ -3,7 +3,7 @@ import os
 
 from acdh_tei_pyutils.tei import TeiReader
 from acdh_tei_pyutils.utils import any_xpath
-from config import HTML_DATA_DIR, LISTBIBL, LISTPERSON
+from config import HTML_DATA_DIR, LISTBIBL, LISTPERSON, LISTPLACE
 
 os.makedirs(HTML_DATA_DIR, exist_ok=True)
 
@@ -29,6 +29,23 @@ print(f"generating {output_file}")
 doc = TeiReader(LISTPERSON)
 data = {}
 for x in doc.any_xpath(".//tei:person[@xml:id]"):
+    label = x.attrib["n"]
+    try:
+        works_written = int(any_xpath(x, "./tei:listBibl[@n]/@n")[0])
+    except IndexError:
+        continue
+    data[label] = works_written
+data = dict(sorted(data.items(), key=lambda item: item[1], reverse=True))
+payload = {"labels": [x for x in data.keys()], "data": [x for x in data.values()]}
+with open(output_file, "w", encoding="utf-8") as fp:
+    json.dump(payload, fp, ensure_ascii=False)
+
+
+output_file = os.path.join(HTML_DATA_DIR, "bibl-per-place.json")
+print(f"generating {output_file}")
+doc = TeiReader(LISTPLACE)
+data = {}
+for x in doc.any_xpath(".//tei:place[@xml:id]"):
     label = x.attrib["n"]
     try:
         works_written = int(any_xpath(x, "./tei:listBibl[@n]/@n")[0])
