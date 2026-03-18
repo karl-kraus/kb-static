da67f7ebf0fb7442c0ac958ea7eec930fe78e927
csae8092 <p.andorfer@gmail.com>
2026-03-18 11:05:10 +0100

    chore: ingest DEBUG env variable; dummy chart page

--- src/charts.ts
+++ src/charts.ts
@@ -1,1 +1,25 @@
-console.log("wow, amazing charts!!!")
+import data from '../html/js/charts/data/bibl-per-author.json'
+import Chart from 'chart.js/auto';
+
+const canvas = document.getElementById('bibl-per-author');
+if (!(canvas instanceof HTMLCanvasElement)) {
+    throw new Error('Canvas element #bibl-per-author not found');
+}
+
+
+new Chart(
+    canvas,
+    {
+        type: 'bar',
+        data: {
+            labels: data.labels,
+            datasets: [
+                {
+                    label: 'Acquisitions by year',
+                    data: data.data
+                }
+            ]
+        }
+    }
+);
+
