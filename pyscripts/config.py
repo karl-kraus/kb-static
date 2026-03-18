b9797bf1960b8d9c7f7d622dc5fa58d93679d0a7
csae8092 <p.andorfer@gmail.com>
2026-03-18 14:09:21 +0100

    feat: adds simple network; ToDo: styling, filtering, interactivity

--- pyscripts/config.py
+++ pyscripts/config.py
@@ -1,7 +1,7 @@
 import os
 
-HTML_DIR = "html"
-HTML_DATA_DIR = os.path.join(HTML_DIR, "js", "charts", "data")
+HTML_DIR = "src"
+HTML_DATA_DIR = os.path.join(HTML_DIR, "data")
 LISTBIBL = os.path.join("data", "indices", "listbibl.xml")
 LISTPERSON = os.path.join("data", "indices", "listperson.xml")
 LISTPLACE = os.path.join("data", "indices", "listplace.xml")
--- src/network.ts
+++ src/network.ts
@@ -1,14 +1,50 @@
 import Graph from "graphology";
+import forceAtlas2 from "graphology-layout-forceatlas2";
+import FA2Layout from "graphology-layout-forceatlas2/worker";
+
 import Sigma from "sigma";
+import data from './data/network.json'
+
 const graph = new Graph();
 
 const graph_container = document.getElementById('visContainer')
+if (!(graph_container instanceof HTMLElement)) {
+    throw new Error("Element #visContainer not found");
+}
+
+const colors = {
+    "year": "#d97706",
+    "bibl": "#1d4ed8",
+    "author": "#10a633",
+    "place": "#9333ea",
+}
+
 graph_container.style.height = "70vh";
-graph_container.style.width  = "50%";
+graph_container.style.width = "90%";
 
 
-graph.addNode("1", { label: "Node 1", x: 0, y: 0, size: 10, color: "blue" });
-graph.addNode("2", { label: "Node 2", x: 1, y: 1, size: 20, color: "red" });
-graph.addEdge("1", "2", { size: 5, color: "purple" });
+for (const node of data.nodes) {
+    if (graph.hasNode(node.id)) continue;
 
+    const isYear = node.type === "year";
+    graph.addNode(node.id, {
+        label: node.label,
+        x: Math.random(),
+        y: Math.random(),
+        color: colors[node.type],
+    });
+}
+
+for (const [source, target] of data.edges) {
+    if (!graph.hasNode(source) || !graph.hasNode(target)) continue;
+    if (graph.hasEdge(source, target)) continue;
+
+    graph.addEdge(source, target, { size: 1, color: "#9ca3af" });
+}
+
+const sensibleSettings = forceAtlas2.inferSettings(graph);
+const fa2Layout = new FA2Layout(graph, {
+    settings: sensibleSettings,
+});
+ fa2Layout.start();
 new Sigma(graph, graph_container);
