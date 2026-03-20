e87c50ee03e7f5e6da552bb3a2c42d2475a2d64f
csae8092 <p.andorfer@gmail.com>
2026-03-20 11:32:16 +0100

    feat(charts): add titles, zoom, reset zoom buttons

--- src/charts.ts
+++ src/charts.ts
@@ -1,23 +1,40 @@
-import biblPerAuthors from './data/bibl-per-author.json' 
-import biblPerPlaces from './data/bibl-per-place.json' 
-import biblPerYears from './data/bibl-per-year.json' 
+import biblPerAuthors from './data/bibl-per-author.json'
+import biblPerPlaces from './data/bibl-per-place.json'
+import biblPerYears from './data/bibl-per-year.json'
 import biblPerOrgs from './data/bibl-per-org.json'
 import Chart from 'chart.js/auto';
+import zoomPlugin from 'chartjs-plugin-zoom';
 
+Chart.register(zoomPlugin);
 const container = document.getElementById("chartCanvas");
-
 const data = {
     biblPerAuthors,
     biblPerPlaces,
     biblPerYears,
     biblPerOrgs
 };
-
+const titles = {
+    biblPerAuthors: "Anzahl der Texte pro Autor",
+    biblPerPlaces: "Anzahl der Texte pro Ort",
+    biblPerYears: "Anzahl der Texte pro Jahr",
+    biblPerOrgs: "Anzahl der Texte pro Verlag"
+};
+const h2_classes = ["text-center", "pt-2"]
+const button_classes = ["btn", "btn-outline-primary", "d-block", "mx-auto", "mb-3"]
 for (const [x, cur_data] of Object.entries(data)) {
     let canvas = document.createElement('canvas')
     canvas.id = x
+    let heading = document.createElement('h2')
+    heading.textContent = titles[x]
+    heading.classList.add(...h2_classes)
+    let button = document.createElement('button')
+    button.textContent = "Zoom zurücksetzen"
+    button.id = `${x}-resetButton`
+    button.classList.add(...button_classes)
+    container.append(heading)
+    container.append(button)
     container.append(canvas)
-    new Chart(
+    let cur_chart = new Chart(
         canvas,
         {
             type: 'bar',
@@ -25,13 +42,33 @@ for (const [x, cur_data] of Object.entries(data)) {
                 labels: cur_data.labels.map(String),
                 datasets: [
                     {
-                        label: 'Texts',
+                        label: 'Texte',
                         data: cur_data.data
                     }
                 ]
+            },
+            options: {
+                plugins: {
+                    zoom: {
+                        zoom: {
+                            wheel: {
+                                enabled: true,
+                            },
+                            pinch: {
+                                enabled: true
+                            },
+                            mode: 'x',
+                        }
+                    }
+                }
             }
         }
     );
+    button.addEventListener('click', () => {
+        if (cur_chart) {
+            cur_chart.resetZoom();
+        }
+    });
 }
 
 
